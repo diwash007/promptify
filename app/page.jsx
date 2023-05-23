74fbeba1d88a8aadf69a7417890a89222f5b2cac
Diwash <diwashdahal75@gmail.com>
2023-05-23 11:47:29 +0545

    feat: add navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,17 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <section className="w-full flex-center flex-col text-center">
+      <h1 className="head_text">
+        Discover and Share
+        <br/>
+        <span className="orange_gradient text-center">AI powered prompts</span>
+      </h1>
+      <p className="description text-center"></p>
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
